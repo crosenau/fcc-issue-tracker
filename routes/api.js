8a779b98879ba619c8b9228fff00f423f455e0b6
Glitch (stormy-calf) <none>
2019-01-30 09:57:38 +0000

    Replaced validateIdString() w/ mongodb.ObjectID.isValid(). Refactored input validation and input sanitizing

--- routes/api.js
+++ routes/api.js
@@ -11,11 +11,11 @@
 const expect = require('chai').expect;
 const ObjectID = require('mongodb').ObjectID;
 
-function verifyRequiredInputs(inputs, required) {
+function verifyRequiredInputs(inputs, requiredFields) {
   let hasRequiredValues = true;
   
-  for (let key of required) {
-    if (!inputs[key] || inputs[key] === '') {
+  for (let field of requiredFields) {
+    if (!inputs[field] || inputs[field] === '') {
       hasRequiredValues = false;
     }
   }
@@ -23,84 +23,64 @@ function verifyRequiredInputs(inputs, required) {
   return hasRequiredValues;
 }
 
-function validateIdString(string) {
-  // Determine if an input string can be converted to a valid mongodb.ObjectID
-  
-  try {
-    return ObjectID(string) instanceof ObjectID;
-  } catch(err) {
-    return false;
-  }
-}
-
-function inputsToQuery(inputs) {
+function inputsToMongoObject(inputs) {
   // Converts a request.body or request.query object to a valid query object for mongod
-  const mongoQuery = {};
+  const mongoObject = {};
   
   for (let key in inputs) {
     switch (key) {
       case '_id':
-        mongoQuery[key] = ObjectID(inputs[key]);
+        mongoObject[key] = ObjectID(inputs[key]);
         break;
       case 'open':
-        mongoQuery[key] = (inputs[key] === 'true');
+        mongoObject[key] = (inputs[key] === 'true');
         break;
       case 'created_on':
-        mongoQuery[key] = new Date(inputs[key]);
+        mongoObject[key] = new Date(inputs[key]);
         break;
       case 'updated_on':
-        mongoQuery[key] = new Date(inputs[key]);
+        mongoObject[key] = new Date(inputs[key]);
         break;
       default:
-        mongoQuery[key] = inputs[key];
+        mongoObject[key] = sanitize(inputs[key]);
     }
   }
   
-  return mongoQuery;
+  return mongoObject;
 }
 
-function sanitizeInputs(inputs) {
-  let sanitized = {};
+function sanitize(input) {
+  // Strip out '<' and '>' characters to prevent HTML injection.
   
-  for (let key in inputs) {
-    if (typeof inputs[key] === 'string') {
-      sanitized[key] = inputs[key]
-        .replace(/\</gm, '&lt')
-        .replace(/\>/gm, '&gt');
-    } else {
-      sanitized[key] = inputs[key];
-    }
-  }
-  
-  return sanitized;
+  return input      
+    .replace(/\</gm, '&lt')
+    .replace(/\>/gm, '&gt');
 }
 
 module.exports = (app, db) => {
   app.route('/api/issues/:project')
   
     .all((req, res, next) => {
-      // If request contains an _id, verify that it can be converted to MongoDb ObjectID
+      // If request contains an _id, verify that it is a valid mongodb.ObjectID
+      
+      const inputSets = [req.query, req.body, req.params];
     
-      let containsValidId;
-    
-      if (Object.keys(req.body).includes('_id')) {
-        containsValidId = validateIdString(req.body._id);
-      } else if (Object.keys(req.query).includes('_id')) {
-        containsValidId = validateIdString(req.query._id);
-      } else {
-        return next();
+      for (let inputs of inputSets) {
+        for (let key in inputs) {
+          if (key === '_id' && !ObjectID.isValid(inputs[key])) {
+            return res.send('_id error');
+          }
+        }
       }
     
-      if (containsValidId) return next();
-    
-      return res.send('_id error');
+      next();
     })
 
     .get((req, res) => {
       const project = req.params.project;
-      const inputObj = sanitizeInputs(inputsToQuery(req.query));
+      const query = inputsToMongoObject(req.query);
     
-      db.collection(`${project}-issues`).find(inputObj).toArray()
+      db.collection(`${project}-issues`).find(query).toArray()
         .then(results => res.json(results))
         .catch(err => {
           console.error(err);
@@ -111,7 +91,7 @@ module.exports = (app, db) => {
     .post((req, res) => {    
       const project = req.params.project;
       const timeStamp = new Date();
-      const inputObj = sanitizeInputs({
+      const document = {
         issue_title: req.body.issue_title,
         issue_text: req.body.issue_text,
         created_on: timeStamp,
@@ -120,13 +100,13 @@ module.exports = (app, db) => {
         assigned_to: req.body.assigned_to || '',
         open: true,
         status_text: req.body.status_text || ''
-      });
+      };
     
       const requiredFields = ['issue_title', 'issue_text', 'created_on'];
     
-      if (!verifyRequiredInputs(inputObj, requiredFields)) return res.send('missing inputs');
+      if (!verifyRequiredInputs(document, requiredFields)) return res.send('missing inputs');
     
-      db.collection(`${project}-issues`).insertOne(inputObj)
+      db.collection(`${project}-issues`).insertOne(document)
         .then(result => res.json(result.ops[0]))
         .catch(err => {
           console.error(err);
@@ -136,23 +116,23 @@ module.exports = (app, db) => {
 
     .put((req, res) => {
       const project = req.params.project;
-      const inputObj = sanitizeInputs(inputsToQuery(req.body));
+      const update = inputsToMongoObject(req.body);
       const requiredFields = ['_id'];
     
       if (
-        !verifyRequiredInputs(inputObj, requiredFields) 
-        || Object.keys(inputObj).length < 2
+        !verifyRequiredInputs(update, requiredFields) 
+        || Object.keys(update).length < 2
       ) {
         return res.send('no updated field sent');
       }
     
     
-      const id = inputObj._id;
+      const id = update._id;
       const timeStamp = new Date();
     
-      inputObj.updated_on = timeStamp;
+      update.updated_on = timeStamp;
     
-      db.collection(`${project}-issues`).findOneAndUpdate({ _id: id }, { $set: {...inputObj}}, { returnOriginal: false })
+      db.collection(`${project}-issues`).findOneAndUpdate({ _id: id }, { $set: {...update}}, { returnOriginal: false })
         .then(result => {
           if (result.lastErrorObject.updatedExisting) {
             res.send('successfully updated');
@@ -168,19 +148,20 @@ module.exports = (app, db) => {
 
     .delete((req, res) => {
       const project = req.params.project;  
-      const inputObj = { _id: ObjectID(req.body._id) };
-    
-      db.collection(`${project}-issues`).deleteOne(inputObj)
+      const filter = { _id: ObjectID(req.body._id) };
+      const id = ObjectID(req.body._id);
+
+      db.collection(`${project}-issues`).deleteOne({ _id: id })
         .then(result => {
           if (result.deletedCount > 0) {
-            return res.send(`deleted ${inputObj._id}`);
+            return res.send(`deleted ${id}`);
           }
         
-          return res.send(`could not delete ${inputObj._id}`);
+          return res.send(`could not delete ${id}`);
         })
         .catch(err => {
           console.error(err);
-          res.send(`could not delete ${inputObj._id}`);
+          res.send(`could not delete ${id}`);
         });
     });
 };
